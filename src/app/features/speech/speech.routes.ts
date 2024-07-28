69e2abd7bacbd536eba08b882d98a2ab0bfd2fbe
d3rdone <19626442+d3rdOne@users.noreply.github.com>
2024-07-28 12:34:29 +0800

    Update page title for each route

--- src/app/features/speech/speech.routes.ts
+++ src/app/features/speech/speech.routes.ts
@@ -3,12 +3,12 @@ import { SpeechResolver } from "../../core/resolvers/speech-resolver.service";
 
 export const speechRoutes: Routes = [
   {
-    title: 'New Speech',
+    title: 'Create Speech',
     path: 'new',
     loadComponent: () => import(`./views/new-speech/new-speech.component`).then(a => a.NewSpeechComponent)
   },
   {
-    title: 'Details',
+    title: 'Speech Details',
     path: ':id',
     resolve: {speechId:  SpeechResolver},
     loadComponent: () => import('./views/speech-details/speech-details.component').then(a => a.SpeechDetailsComponent)
